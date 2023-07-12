5631bc5fa10cc35b92b5bf587528a39cabb11ce3
Batool Fatima <batoolfatima2135@gmail.com>
2023-07-12 18:58:26 +0500

    Add form to create new books

---
+++ src/components/AddBook.jsx
@@ -0,0 +1,9 @@
+const AddBook = () => (
+  <form>
+    <h1>Create New Book</h1>
+    <input type="text" placeholder="Book title" />
+    <input type="text" placeholder="Author" />
+    <input type="submit" value="Add book" />
+  </form>
+);
+export default AddBook;
