dd75462b2da8d0b28456e16c44414430ccfdcea3
Batool Fatima <batoolfatima2135@gmail.com>
2023-07-12 18:43:53 +0500

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Routes, Route } from 'react-router-dom';
+import Home from './Routes/Home';
+import Categories from './Routes/Categories';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h>Book store</h>
-    </div>
+    <Routes>
+      <Route path="/" element={<Navbar />}>
+        <Route index element={<Home />} />
+        <Route path="/categories" element={<Categories />} />
+      </Route>
+    </Routes>
   );
 }
 
