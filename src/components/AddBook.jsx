3d86effc2c480f37f7c5edc37a546004d63a4f55
Batool Fatima <batoolfatima2135@gmail.com>
2023-07-14 23:50:46 +0500

    Add <button> component to add new book in store

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,9 +1,37 @@
-const AddBook = () => (
-  <form>
-    <h1>Create New Book</h1>
-    <input type="text" placeholder="Book title" />
-    <input type="text" placeholder="Author" />
-    <input type="submit" value="Add book" />
-  </form>
-);
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../Redux/Book/bookSlice';
+
+const AddBook = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuther] = useState('');
+  const dispatch = useDispatch();
+  const addNewBook = () => {
+    const newBook = {
+      item_id: uuidv4(),
+      title,
+      author,
+    };
+    dispatch(addBook(newBook));
+  };
+  return (
+    <form>
+      <h1>Create New Book</h1>
+      <input
+        type="text"
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+        placeholder="Book title"
+      />
+      <input
+        type="text"
+        placeholder="Author"
+        onChange={(e) => setAuther(e.target.value)}
+        value={author}
+      />
+      <button type="button" onClick={addNewBook}>Add Book</button>
+    </form>
+  );
+};
 export default AddBook;
