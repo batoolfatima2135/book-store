d361e3ac829d65ff5cb1232eda4122bbe1ead2dc
Batool Fatima <batoolfatima2135@gmail.com>
2023-07-14 21:51:37 +0500

    Remove linter error

--- src/Redux/index.js
+++ src/Redux/index.js
@@ -1,10 +1,10 @@
+import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from '../App';
 import reportWebVitals from '../reportWebVitals';
-import { Provider } from 'react-redux';
 import Store from './Store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
