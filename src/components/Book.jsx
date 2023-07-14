83142c4425f897a6bb3de6e60e38ecf0e341def2
Batool Fatima <batoolfatima2135@gmail.com>
2023-07-14 22:43:18 +0500

    vent handler that removes a book from store

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,19 +1,29 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../Redux/Book/bookSlice';
 
-const Book = ({ title, author }) => (
-  <div>
-    <p>
-      {title}
-      {' '}
-      by
-      {' '}
-      {author}
-    </p>
-    <button type="button">Delete</button>
-  </div>
-);
+const Book = ({
+  title, author, category, id,
+}) => {
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <p>
+        {title}
+        {' '}
+        by
+        {' '}
+        {author}
+      </p>
+      <p>{category}</p>
+      <button type="button" onClick={() => dispatch(removeBook(id))}>Delete</button>
+    </div>
+  );
+};
 export default Book;
 Book.propTypes = {
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
 };
