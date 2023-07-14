ebd5b558bb66e728edfa64e084ed33b1de2f0c31
Batool Fatima <batoolfatima2135@gmail.com>
2023-07-14 21:41:04 +0500

    Define a slice of state for books

---
+++ src/Redux/Book/bookSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  books: [],
+};
+
+const bookSlice = createSlice({
+  name: 'book',
+  initialState,
+  reducers: {
+    addBook: (state, action) => {
+      state.books.push(action.payload);
+    },
+    removeBook: (state, action) => {
+      state.books = state.books.filter((book) => book !== action.payload);
+    },
+  },
+});
+
+export const { removeBook, addBook } = bookSlice.actions;
+export default bookSlice.reducer;
