298311279b515ede4e8452132898ea4172c09beb
Batool Fatima <batoolfatima2135@gmail.com>
2023-07-14 22:42:55 +0500

    Add books from redux store in bookList Component

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -3,11 +3,18 @@ import Book from './Book';
 
 const BookList = () => {
   const List = useSelector((state) => state.booksList.books);
-  console.log(List);
   return (
     <>
       {
-       List.map((book) => <Book title={book.title} key={book.item_id} author={book.author} />)
+       List.map((book) => (
+         <Book
+           title={book.title}
+           key={book.item_id}
+           id={book.item_id}
+           author={book.author}
+           category={book.category}
+         />
+       ))
     }
     </>
 
