86c280c025a500a0a7190ab75351c3bf53ee7228
Batool Fatima <batoolfatima2135@gmail.com>
2023-07-15 21:48:58 +0500

    Add Api call action

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,8 +1,14 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import Book from './Book';
+import { fetchBooks } from '../Redux/Book/bookSlice';
 
 const BookList = () => {
   const List = useSelector((state) => state.booksList.books);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchBooks());
+  });
   return (
     <>
       {
