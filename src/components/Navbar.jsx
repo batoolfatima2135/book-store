dee1e1a4bb558eb1faebca5cf1a723238acca832
Batool Fatima <batoolfatima2135@gmail.com>
2023-07-17 13:24:05 +0500

    Add styles to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,24 @@
 import { NavLink, Outlet } from 'react-router-dom';
+import '../styles/nav.css';
 
 const Navbar = () => (
-  <div>
-    <nav>
-      <NavLink to="/">Books</NavLink>
-      <NavLink to="/categories">Categories</NavLink>
-    </nav>
+  <div className="container">
+    <div className="panel-bg row align-items-center my-4">
+      <span className="Bookstore-CMS Text-Style-3 col-3">
+        Bookstore CMS
+      </span>
+      <span className="BOOKS Text-Style-6 col-1">
+        <NavLink className="link" to="/">BOOKS</NavLink>
+      </span>
+      <span className="CATEGORIES Text-Style-6 col-2">
+        <NavLink className="link" to="/categories">CATEGORIES</NavLink>
+      </span>
+      <span className="col-1 ms-auto">
+        <div className="Oval d-flex align-items-center justify-content-center rounded-circle">
+          <i className="fas fa-user" />
+        </div>
+      </span>
+    </div>
     <Outlet />
   </div>
 );
