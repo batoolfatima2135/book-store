39cd34f32a4eb4d592e6ebb5c5675c090922bc1c
Batool Fatima <batoolfatima2135@gmail.com>
2023-07-17 17:37:59 +0500

    Add category state

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,22 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
-import { addBook } from '../Redux/Book/bookSlice';
+import { addBook, fetchBooks } from '../Redux/Book/bookSlice';
 
 const AddBook = () => {
   const [title, setTitle] = useState('');
   const [author, setAuther] = useState('');
+  const [category, setCategory] = useState('');
   const dispatch = useDispatch();
   const addNewBook = () => {
     const newBook = {
       item_id: uuidv4(),
       title,
       author,
+      category,
     };
     dispatch(addBook(newBook));
     setAuther('');
     setTitle('');
+    setCategory('');
   };
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [dispatch]);
   return (
     <form>
       <h1>Create New Book</h1>
@@ -31,6 +37,12 @@ const AddBook = () => {
         placeholder="Author"
         onChange={(e) => setAuther(e.target.value)}
         value={author}
+      />
+      <input
+        type="text"
+        placeholder="Category"
+        onChange={(e) => setCategory(e.target.value)}
+        value={category}
       />
       <button type="button" onClick={addNewBook}>Add Book</button>
     </form>
